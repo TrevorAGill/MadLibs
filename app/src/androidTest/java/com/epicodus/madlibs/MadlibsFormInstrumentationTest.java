74699ac34c1656ae99baff4d08c2febe930fe2d4
wedaft <trevor.a.gill@gmail.com>
2017-10-10 13:07:23 -0700

    pass first espresso test

--- app/src/androidTest/java/com/epicodus/madlibs/MadlibsFormInstrumentationTest.java
+++ app/src/androidTest/java/com/epicodus/madlibs/MadlibsFormInstrumentationTest.java
@@ -1,16 +1,25 @@
 package com.epicodus.madlibs;
 
 
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
 import org.junit.Test;
 
+import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 public class MadlibsFormInstrumentationTest {
+    @Rule
+    public ActivityTestRule<MadlibsForm> activityTestRule =
+            new ActivityTestRule<>(MadlibsForm.class);
+
     @Test
     public void validateEditText() {
-        onView(withId(R.id.locationEditText)).perform(typeText("Portland"))
-                .check(matches(withText("Portland")));
+        onView(withId(R.id.noun1)).perform(typeText("test"))
+                .check(matches(withText("test")));
     }
 }
