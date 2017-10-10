2dd84d4afd17f94a39adffcff47aafbcacae5434
Guest User <Guest@Epicodus-3D.local>
2017-10-10 10:17:29 -0700

    Initial commit

---
+++ app/src/main/java/com/epicodus/madlibs/MainActivity.java
@@ -0,0 +1,36 @@
+package com.epicodus.madlibs;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @Bind(R.id.island) TextView mIsland;
+    @Bind(R.id.bunderdoods) TextView mBunderdoods;
+    @Bind(R.id.textView3) TextView textView3;
+    @Bind(R.id.find_madlibs_button) Button mfindMadLibsButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mfindMadLibsButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v == mfindMadLibsButton) {
+            Intent intent = new Intent(MainActivity.this, MadlibsForm.class);
+            startActivity(intent);
+        }
+    }
+}
